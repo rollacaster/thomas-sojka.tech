d687428b80982264cc5407b766ffc905db255f21
Thomas Sojka <thomas.sojka@comsysto.com>
2018-11-11 12:29:55 +0100

    Publish "Compose all the things"

--- src/components/talks/index.js
+++ src/components/talks/index.js
@@ -5,25 +5,49 @@ import { rhythm } from '../../utils/typography'
 const TalkIndex = ({ talks }) => (
   <div>
     <h1>Talks</h1>
-    <h3
-      style={{
-        marginBottom: rhythm(1 / 4),
-      }}
-    >
-      <a
-        style={{ boxShadow: 'none' }}
-        href={'https://www.youtube.com/watch?v=ae_3svi5Eg0'}
-        target="_blank"
+    <div>
+      <h3
+        style={{
+          marginBottom: rhythm(1 / 4),
+        }}
       >
-        Webscraping with algebraic structures in JS
-      </a>
-    </h3>
-    <small>26 October, 2017</small>
-    <p>
-      Lighting Talk about scraping the{' '}
-      <a href="https://reactiveconf.com">ReactiveConf</a> Lightning Talk
-      Proposals with Functors, Monads and Traversables.
-    </p>
+        <a
+          style={{ boxShadow: 'none' }}
+          href={'https://www.youtube.com/watch?v=juMLwOTxnvw'}
+          target="_blank"
+        >
+          Compose all the things
+        </a>
+      </h3>
+      <small>29 October, 2018</small>
+      <p>
+        Functional composition can help to build flexible and readable APIs. It
+        is a concept that can be translated into a variety of programming
+        languages and domains. This talks explores functional composition
+        through the lens of a JavaScript Frontend Developer.
+      </p>
+    </div>
+    <div>
+      <h3
+        style={{
+          marginBottom: rhythm(1 / 4),
+        }}
+      >
+        <a
+          style={{ boxShadow: 'none' }}
+          href={'https://www.youtube.com/watch?v=ae_3svi5Eg0'}
+          target="_blank"
+        >
+          Webscraping with algebraic structures in JS
+        </a>
+      </h3>
+      <small>26 October, 2017</small>
+      <p>
+        Lighting Talk about scraping the{' '}
+        <a href="https://reactiveconf.com">ReactiveConf</a> Lightning Talk
+        Proposals with Functors, Monads and Traversables.
+      </p>
+    </div>
   </div>
 )
 
