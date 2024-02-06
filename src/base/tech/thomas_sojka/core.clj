bb4c432df28278b222bac553df98bdc30857f459
Thomas Sojka <thsojka@web.de>
2024-02-06 09:16:28 +0100

    Update about & now

--- src/base/tech/thomas_sojka/core.clj
+++ src/base/tech/thomas_sojka/core.clj
@@ -3,6 +3,7 @@
    [babashka.fs :as fs]
    [clojure.java.io :as io]
    [tech.thomas-sojka.data :as data]
+   [tech.thomas-sojka.org-parser-meta :as org-parser-meta]
    [tech.thomas-sojka.pages :as pages])
   (:import
    (java.time Instant)))
@@ -14,22 +15,25 @@
                           (io/file "resources/content"))
                 (remove #(.isDirectory %))))
 
-(defn- org-file? [file]
-  (re-find #".org$" (.getName file)))
-(def content-files (filter org-file? files))
+(defn- content-file? [file]
+  (and (re-find #".org$" (.getName file))
+       (not= (:hide (org-parser-meta/parse file)) "true")))
+(def content-files (filter content-file? files))
 (def nav-links (data/nav-links content-files))
 (def content (data/content content-files))
 
 (defn build  [{:keys [files target-folder] :or {target-folder "public"}}]
   (prn "Build:" (count files))
   (fs/copy-tree "resources/images" "public/images" {:replace-existing true})
-  (let [[content-files resource-files] ((juxt filter remove) org-file? files)]
-    (doseq [{:keys [path content]} (pages/generate {:last-build-date (Instant/now)
-                                                    :content-files content-files
-                                                    :content content
-                                                    :nav-links nav-links
-                                                    :resource-files resource-files
-                                                    :target-folder target-folder})]
+  (fs/copy-tree "resources/videos" "public/videos" {:replace-existing true})
+  (let [[content-files resource-files] ((juxt filter remove) content-file? files)]
+    (doseq [{:keys [path content]}
+            (pages/generate {:last-build-date (Instant/now)
+                             :content-files content-files
+                             :content content
+                             :nav-links nav-links
+                             :resource-files resource-files
+                             :target-folder target-folder})]
       (io/make-parents path)
       (if (string? content)
         (spit path content)
