9ff219a100c2fcc51b101e0a6310623325766d15
Thomas Sojka <thsojka@web.de>
2024-02-20 16:22:34 +0100

    Restructure how pages are built

--- src/base/tech/thomas_sojka/core.clj
+++ src/base/tech/thomas_sojka/core.clj
@@ -9,45 +9,57 @@
   (:import
    (java.time Instant)))
 
-
-(def files (->> (tree-seq (fn [f] (.isDirectory f))
+(defn- read-all-file-paths [folder]
+  (->> (tree-seq (fn [f] (.isDirectory f))
                           (fn [f] (->> (file-seq f)
                                       (remove #(.isDirectory %))))
-                          (io/file "resources/content"))
+                          (io/file folder))
                 (remove #(.isDirectory %))))
 
-(defn- content-file? [file]
-  (and (re-find #".org$" (.getName file))
-       (not= (:hide (org-parser-meta/parse file)) "true")))
-(def content-files (filter content-file? files))
-(def nav-links (data/nav-links content-files))
-(def content (data/content content-files))
 
-(defn build  [{:keys [files target-folder] :or {target-folder "public"}}]
+(def content-file-paths (read-all-file-paths "resources/content"))
+
+(defn- content-file? [file] (re-find #".org$" (.getName file)))
+
+(def content-files (filter content-file? content-file-paths))
+(def nav-links (data/nav-links content-files))
+
+
+(defn- copy-to-target [path content]
+  (io/make-parents path)
+  (if (string? content)
+    (spit path content)
+    (io/copy content (io/file path))))
+
+(def target-folder "public")
+
+(defn build-page [file]
+  (cond
+    (and (content-file? file) (:content-type (org-parser-meta/parse file)))
+
+    (pages/content-file->html file target-folder nav-links)
+    (= file "resources/styles/glow.css") (pages/syntax-colouring-css file)
+
+    :else (pages/copy-resource-file file target-folder)))
+
+(defn build  [{:keys [files]}]
   (prn "Build:" (count files))
   (fs/copy-tree "resources/images" "public/images" {:replace-existing true})
   (fs/copy-tree "resources/videos" "public/videos" {:replace-existing true})
   (doseq [locale i18n/locales]
     (reset! i18n/locale locale)
-    (let [[content-files resource-files] ((juxt filter remove) content-file? files)]
-      (doseq [{:keys [path content]}
-              (pages/generate {:last-build-date (Instant/now)
-                               :content-files content-files
-                               :content content
-                               :nav-links nav-links
-                               :resource-files resource-files
-                               :target-folder (cond-> target-folder
-                                                (not= locale :en) (str "/" (name locale) "/"))})]
-        (io/make-parents path)
-        (if (string? content)
-          (spit path content)
-          (io/copy content (io/file path)))))))
+    (let [last-build-date (Instant/now)
+          content (data/content content-files)]
+      (doseq [{:keys [path content]} [(pages/home-page target-folder nav-links content)
+                                      (pages/rss-xml target-folder last-build-date content)
+                                      (map build-page (conj files (fs/file "resources/styles/glow.css")))]]
+        (copy-to-target path content)))))
 
 (defonce new-build (atom false))
 
 (defn main [_]
-  (build {:files files})
+  (build {:files content-file-paths})
   (reset! new-build true))
 
 (comment
-  (build {:files files :target-folder "public"}))
+  (build {:files content-file-paths :target-folder "public"}))
