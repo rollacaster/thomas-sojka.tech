9f9c9b7543db7a4de85338f274385cd6a4e322e7
Thomas Sojka <thsojka@web.de>
2024-03-26 18:49:40 +0100

    Add basic new project section

--- src/base/tech/thomas_sojka/i18n.clj
+++ src/base/tech/thomas_sojka/i18n.clj
@@ -16,9 +16,38 @@
                  :hero/link-1 "writing"
                  :hero/link-2 "talking"
                  :hero/link-3 "building"
-                 :main/blogs "Posts"
+                 :main/posts "Posts"
+                 :main/posts-description "A selection of the articles I wrote in the past. Writing
+about something is the best way to truly learn something. It helps to
+structure thoughts and uncover areas that are worth exploring deeper."
+                 :main/posts-1-description "You can use your development skills to improve the things you develop. Let's explore this productivity cycle."
+                 :main/posts-2-description "Telling the story of GrootLapse timelapse camera for plant watching in built."
+                 :main/posts-3-description "When I first learned about macros, I recognized how powerful they are but had no idea what to build with them. Thankfully this changed."
                  :main/show-publications "Show all publications"
                  :main/side-projects "Side Projects"
+                 :main/side-projects-description "I am really grateful that I could make my hobby my
+profession. But it's still a hobby for me as well and here are projects that I
+built for others or myself."
+                 :main/side-projects-1-description "One of my rare projects that
+                 gifted me more lifetime than it took to build it. Shopping
+                 Cards helps me to plan my meals and creates shopping lists for
+                 me. The shopping lists are sorted by the aisles of my local
+                 supermarket. It also helped me to reduce my food waste
+                 significantly. Technically is just yet another CRUD-App but it
+                 had a big impact on my life, which makes it my favorite side
+                 project."
+                 :main/side-projects-2-description "I built a time-lapse camera
+                 to track paprikas growing in a hydroponic system. This was one
+                 of my first physical projects and taught me about video
+                 processing as well. Although the final paprikas didn't taste
+                 that great, it was fun to watch them grow and to be able to
+                 touch the thing I built."
+                 :main/side-projects-3-description "My first serious attempt to
+                 build something not only for one person but to help others with
+                 a problem that was nagging me as well. It's a snippet
+                 collection to build charts in Clojure using the D3.js library.
+                 While building it, I discovered many practical use cases of
+                 Clojure macros for the first time."
                  :main/talks "Talks"
                  :about/title "Hi 👋"
                  :about/paragraph-1 "My name is Thomas. I am a programmer living in Munich and I am a freelance software engineer."
@@ -49,9 +78,44 @@
                  :hero/link-1 "schreiben"
                  :hero/link-2 "sprechen"
                  :hero/link-3 "bauen"
-                 :main/blogs "Beiträge"
+                 :main/posts "Beiträge"
+                 :main/posts-description "Eine Auswahl der Artikel, die ich in
+                 der Vergangenheit geschrieben habe. Über etwas zu schreiben ist
+                 der beste Weg, um etwas wirklich zu lernen. Es hilft, Gedanken
+                 zu strukturieren und Bereiche aufzudecken, die es wert sind,
+                 näher erkundet zu werden."
+                 :main/posts-1-description "Du kannst deine Entwicklerfähigkeiten nutzen, um die von dir erstellten Dinge zu verbessern. Lass uns diesen Produktivitätszyklus erkunden."
+                 :main/posts-2-description "Erzählt die Geschicht der GrootLapse-Zeitrafferkamera, diese beobachtet Pflanzen während sie wachsen."
+                 :main/posts-3-description "Als ich zum ersten Mal von Makros gehört habe, erkannte ich, wie mächtig sie sind, wusste aber nicht, was ich damit bauen sollte. Zum Glück hat sich das geändert."
                  :main/show-publications "Alle Publikationen"
                  :main/side-projects "Projekte"
+                 :main/side-projects-description "Ich bin wirklich dankbar, dass
+                 ich mein Hobby zu meinem Beruf machen konnte. Aber es ist für
+                 mich immer noch auch ein Hobby, und hier sind Projekte, die ich
+                 für andere oder für mich selbst gebaut habe."
+                 :main/side-projects-1-description
+                 "Eines meiner seltenen Projekte, das mir mehr Lebenszeit
+                 geschenkt hat, als es gebraucht hat, um es zu bauen. Shopping
+                 Cards hilft mir, mein Essen zu planen und erstellt
+                 Einkaufslisten für mich. Die Einkaufslisten sind nach den
+                 Gängen meines Supermarkts nebanan sortiert. Es hat mir auch
+                 geholfen, meine Lebensmittelverschwendung zu reduzieren.
+                 Technisch gesehen ist es nur eine weitere CRUD-Anwendung, aber
+                 es hatte einen großen Einfluss auf mein Leben, weshalb es mein
+                 Lieblings-Side-Projekt ist."
+                 :main/side-projects-2-description
+                 "Ich habe eine Zeitraffer-Kamera gebaut, um Paprikas in einem
+Hypdroponischen System beim wachsen zu zusehen. Das war eines meiner ersten
+physischen Projekte und hat mir auch einiges über Videobearbeitung
+beigebracht. Obwohl die endgültigen Paprikas nicht sehr gut geschmeckt haben, hat
+es Spaß gemacht sie wachsen zu sehen."
+                 :main/side-projects-3-description
+                 "Mein erster ernsthafter Versuch, etwas zu bauen, das nicht nur
+                 für eine Person gedacht ist, sondern anderen mit einem Problem
+                 zu helfen, das mich auch plagte. Es ist eine
+                 Code-Schnipselsammlung zum Erstellen von Diagrammen in Clojure
+                 mit der D3.js-Bibliothek. Beim Bauen habe ich zum ersten Mal
+                 viele praktische Anwendungsfälle von Clojure-Makros entdeckt."
                  :main/talks "Vorträge"
                  :about/title "Hallo 👋"
                  :about/paragraph-1 "Mein Name ist Thomas. Ich bin ein freiberuflicher Programmierer aus München."
--- src/base/tech/thomas_sojka/pages/home.clj
+++ src/base/tech/thomas_sojka/pages/home.clj
@@ -12,6 +12,42 @@
     [:div.flex.justify-between.py-2.px-6.bg-gray-200.rounded-lg.rounded-t-none
      [:span.text-gray-700 (i18n/format-date date)]]]])
 
+(defn- wave
+  ([]
+   (wave {}))
+  ([{:keys [class]}]
+   [:svg.fill-gray-200.w-screen
+    {:preserveAspectRatio "none" :width "1440" :height "32" :viewbox "0 0 1440 32" :xmlns "http://www.w3.org/2000/svg"
+     :class class}
+    [:path
+     {:d "M0 0.000244141C0 0.000244141 133 15.5005 178.5 15.5002C224 15.5 315 0.000244141 360 0.000244141C405 0.000244141 496 15.5002 543 15.5002C590 15.5002 669.5 0.000244141 720 0.000244141C770.5 0.000244141 848 15.5002 904 15.5002C960 15.5002 1031.5 0.000244141 1080 0.000244141C1128.5 0.000244141 1196.5 15.5002 1254.5 15.5002C1312.5 15.5002 1440 0.000244141 1440 0.000244141V32.0002H0V0.000244141Z"}]]))
+
+(defn- side-project [{:keys [title description images flipped]}]
+  (let [text-content [:div.px-6.lg:px-0 {:class "lg:w-1/2"}
+                      [:div.text-2xl.mb-8 title]
+                      [:p.mb-12 description]
+                      #_[:div.flex.justify-between.px-16
+                       [:a.bg-gray-600.text-center.text-white.p-3.lg:p-4.shadow-xl.rounded-lg
+                        "Show code"]
+                       [:a.bg-gray-600.text-center.text-white.p-3.lg:p-4.shadow-xl.rounded-lg
+                        "Learn more"]]]]
+    (into [:div.max-w-5xl.mx-auto.flex.gap-8.flex-wrap.lg:flex-nowrap
+           {:class (if flipped "py-32" "py-16")}]
+          (if flipped
+            [images
+             text-content]
+            [text-content
+             images]))))
+
+(defn- section-title [{:keys [title description]}]
+  [:section.relative
+    [:div.pt-16.pb-24.bg-gray-500.text-white
+     [:div.max-w-5xl.mx-auto.px-6.lg:px-0
+      [:h2.mb-2.font-normal.text-4xl title]
+      [:p.max-w-xl description]]]
+    [:div.absolute.bottom-0.overflow-hidden.max-w-full.translate-y-px
+     (wave)]])
+
 (defn main [{:keys [blogs]}]
   [:div
    [:div#main.absolute.w-full.h-screen {:class "mt-[68px]"}]
@@ -32,22 +68,13 @@
                  (i18n/translate :hero/mail-subject) "&body="
                  (i18n/translate :hero/mail-body))}
      (i18n/translate :hero/cta)]
-    [:div.absolute.bottom-0.w-screen.max-w-full
-     [:svg.fill-gray-200
-      {:viewbox "0 0 1440 64" :xmlns "http://www.w3.org/2000/svg"}
-      [:path
-       {:d "m 0.88987591,22.913181
-c 0,0 89.73411409,17.861168 156.60917409,20.269747 39.71097,1.430235 147.67411,-28.420126 204.52513,-28.386591 46.45288,0.0274 222.10347,26.038055 268.81529,26.364725 48.05738,0.33608 283.76326,-23.270731 336.12069,-24.153131 37.55854,-0.632988 215.98834,19.136613 274.72684,21.222866 31.2225,1.108947 201.8725,-20.180313 201.8725,-20.180313
-L 1440,64
-H 0
-Z"}]]]]
+    [:div.absolute.bottom-0.overflow-hidden.max-w-full
+     (wave)]]
 
-
-
-   [:section.flex-1.relative.mb-32
-    [:div.bg-gray-200.py-16.md:py-32
-     [:div.flex.md:gap-16.justify-center.items-center.flex-wrap.md:flex-nowrap.px-6.lg:px-0.max-w-5xl.mx-auto.
-      [:div
+   [:section.flex-1.relative
+    [:div.bg-gray-200.py-16
+     [:div.flex.md:gap-16.justify-between.items-center.flex-wrap.md:flex-nowrap.px-6.lg:px-0.max-w-5xl.mx-auto.
+      [:div.max-w-xl
        [:p.mt-0.text-4xl (i18n/translate :about/title)]
        [:p (i18n/translate :about/paragraph-1)]
        [:p (i18n/translate :about/paragraph-2)]
@@ -55,34 +82,72 @@ Z"}]]]]
       [:div
        [:figure.w-60 {:class "saturate-[.70]"}
         [:a {:href "images/me.png" :alt (i18n/translate :about/image-alt)}
-         [:img.p-0.float-left.rounded {:src "images/me.png" :alt (i18n/translate :about/image-alt)}]]]]]]
-    [:div.absolute.w-screen.max-w-full.rotate-180
-     [:svg.fill-gray-200
-      {:viewbox "0 0 1440 64" :xmlns "http://www.w3.org/2000/svg"}
-      [:path
-       {:d "m 0.88987591,22.913181
-c 0,0 89.73411409,17.861168 156.60917409,20.269747 39.71097,1.430235 147.67411,-28.420126 204.52513,-28.386591 46.45288,0.0274 222.10347,26.038055 268.81529,26.364725 48.05738,0.33608 283.76326,-23.270731 336.12069,-24.153131 37.55854,-0.632988 215.98834,19.136613 274.72684,21.222866 31.2225,1.108947 201.8725,-20.180313 201.8725,-20.180313
-L 1440,64
-H 0
-Z"}]]]]
+         [:img.p-0.float-left.rounded {:src "images/me.png" :alt (i18n/translate :about/image-alt)}]]]]]]]
 
-   [:section.max-w-5xl.mx-auto.py-16.flex-1.px-6.lg:px-0
-    (into [:div.flex.flex-col.gap-8.md:grid.md.md:gap-0
-           {:class "grid-cols-[1fr_0.2fr_1fr_3fr_1fr_0.5fr_1fr_4fr_0.5fr_1fr]
+   (section-title
+    {:title (i18n/translate :main/posts)
+     :description (i18n/translate :main/posts-description)})
+
+   [:section
+    [:div.bg-gray-200.py-8.md:py-16
+     (into [:div.flex.flex-col.gap-8.md:grid.md.md:gap-0.max-w-5xl.mx-auto
+            {:class "grid-cols-[1fr_0.2fr_1fr_3fr_1fr_0.5fr_1fr_4fr_0.5fr_1fr]
  grid-rows-[1fr_3fr_1fr_0.5fr_2fr_1fr_1fr]
 md:height-[520px]"}]
-          (let [[post1 post2 post3] (->> blogs
-                                         (sort-by :date)
-                                         reverse
-                                         (take 3))]
-            [[:h2#writing.mb-2.font-normal.text-4xl.col-start-1.col-end-3
-              (i18n/translate :main/blogs)]
-             [:div.col-start-2.col-end-6.row-start-2.row-end-5
-              (blog post1)]
-             [:div.col-start-8.col-end-11.row-start-1.row-end-3
-              (blog post2)]
-             [:div.col-start-7.col-end-9.row-start-4.row-end-8
-              (blog post3)]
-             [:a.bg-gray-600.text-center.text-white.p-3.lg:p-4.shadow-xl.rounded-lg.col-start-4.col-end-5.row-start-6.row-end-7
-              {:href (if (= @i18n/locale :en) "/publications.html" "/de/publications.html")}
-              (i18n/translate :main/show-publications)]]))]])
+           (let [[post1 post2 post3] (->> blogs
+                                          (sort-by :date)
+                                          reverse
+                                          (take 3))]
+             [[:div.col-start-2.col-end-6.row-start-2.row-end-5
+               (blog (merge post1
+                            {:description (i18n/translate :main/posts-1-description)}))]
+              [:div.col-start-8.col-end-11.row-start-1.row-end-3
+               (blog (merge post2
+                            {:description (i18n/translate :main/posts-2-description)}))]
+              [:div.col-start-7.col-end-9.row-start-4.row-end-8
+               (blog (merge post3
+                            {:description (i18n/translate :main/posts-3-description)}))]
+              [:a.bg-gray-600.text-center.text-white.p-3.lg:p-4.shadow-xl.rounded-lg.col-start-4.col-end-5.row-start-6.row-end-7
+               {:href (if (= @i18n/locale :en) "/publications.html" "/de/publications.html")}
+               (i18n/translate :main/show-publications)]]))]]
+
+   (section-title
+    {:title (i18n/translate :main/side-projects)
+     :description (i18n/translate :main/side-projects-description)})
+
+   [:section.bg-gray-200.relative
+    (side-project
+     {:title"Shopping Cards"
+      :description (i18n/translate :main/side-projects-1-description)
+      :images
+      [:div.flex.gap-4.px-6.lg:px-0 {:class "lg:w-1/2"}
+       [:img.min-w-0
+        {:src "images/home/shopping-cards-weekplan.png" :alt "Screenshot of the Weekplan"}]
+       [:img.min-w-0.hidden.md:block
+        {:src "images/home/shopping-cards-ingredients.png" :alt "Screenshot of the Ingredients Selection"}]]})]
+
+   [:section.relative
+    [:div.absolute.top-0.rotate-180.overflow-hidden.max-w-full
+     (wave)]
+    (side-project
+     {:flipped true
+      :title "Grootlapse"
+      :description (i18n/translate :main/side-projects-2-description)
+      :images
+      [:div.flex.gap-4.px-6.lg:px-0
+       {:class "lg:w-1/2"}
+       [:img.min-w-0
+        {:src "images/home/grootlapse-1.png" :alt "Picture fo the time-lapse camera"}]
+       [:img.hidden.md:block.min-w-0
+        {:src "images/home/grootlapse-2.png" :alt "Screenshot of the main menu"}]]})
+    [:div.absolute.bottom-0.overflow-hidden.max-w-full
+     (wave)]]
+
+   [:section.bg-gray-200.relative
+    (side-project
+     {:title "hiccup-d3"
+      :description (i18n/translate :main/side-projects-3-description)
+      :images
+      [:div.px-6.lg:px-0 {:class "lg:w-1/2"}
+       [:img.min-w-0
+        {:src "images/home/hiccup-d3.png" :alt "Screenshot of the hiccup-d3 website. Showing multiple charts"}]]})]])
